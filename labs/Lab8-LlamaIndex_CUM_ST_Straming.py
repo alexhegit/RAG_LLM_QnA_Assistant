39f6a52f18e13aebd2d3329956b7a271c4b8b95d
Alex He <heye_dev@163.com>
2024-05-28 14:21:52 +0800

    lab8 streaming output base on lab7
    Signed-off-by: Alex He <heye_dev@163.com>

---
+++ labs/Lab8-LlamaIndex_CUM_ST_Straming.py
@@ -0,0 +1,107 @@
+# Lab8 Use streamlit to run RAG_LLM Q&A, use streaming output base one Lab7-LlamaIndex_CUM_ST.py
+
+# Usage: At the dir of it and run the command "streamlit run ./Lab8-LlamaIndex_CUM_ST_Streaming.py"
+
+# Question Examples
+"""
+What's the steps to SWITCHING THE DAYTIME RUNNING LAMPS ON AND OFF and which pages show it?
+
+
+Which pages could get the detials about Exterior Lighting Control?
+"""
+
+import os
+# Enalbe 780M with ROCm
+os.environ['HSA_OVERRIDE_GFX_VERSION'] = '11.0.0'
+
+import streamlit as st
+#import openai
+#from llama_index.llms.openai import OpenAI
+
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, ServiceContext, StorageContext, Settings
+from llama_index.core.node_parser import SentenceSplitter
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.llms.ollama import Ollama
+
+import chromadb
+from llama_index.vector_stores.chroma import ChromaVectorStore
+
+# Set embedding model
+# Please download it ahead running this lab by "ollama pull nomic-embed-text"
+Settings.embed_model = OllamaEmbedding(model_name="nomic-embed-text")
+
+# Set ollama model
+Settings.llm = Ollama(model="llama3", request_timeout=200.0, temperature=0.6)
+
+st.set_page_config(page_title="Chat with the Streamlit docs, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+#openai.api_key = st.secrets.openai_key
+st.title("Chat with the Streamlit docs, powered by LlamaIndex 💬🦙")
+st.info("Check out the full tutorial to build this app in our [blog post](https://blog.streamlit.io/build-a-chatbot-with-custom-data-sources-powered-by-llamaindex/)", icon="📃")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about User Car Manual!"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+def load_data():
+    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+        service_context = ServiceContext.from_defaults(llm=Settings.llm, 
+                                                           embed_model=Settings.embed_model, 
+                                                           system_prompt="You are an expert on the Car User Manual and your job is to answer technical questions. Assume that all questions are related to the User Car Manual. Keep your answers technical and based on facts – do not hallucinate features.")
+        
+        if not os.path.exists("./chroma_db/CUM_en_db"):
+            # initialize client
+            db = chromadb.PersistentClient(path="./chroma_db/CUM_en_db")
+            # get collection
+            chroma_collection = db.get_or_create_collection("CUM_en_db")
+            # assign chroma as the vector_store to the context
+            vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+            storage_context = StorageContext.from_defaults(vector_store=vector_store)
+            # Load data
+            docs = SimpleDirectoryReader(input_files=["../data/FordUM.pdf"]).load_data()
+            # Build vector index per-document
+            index = VectorStoreIndex.from_documents(
+                docs,
+                service_context=service_context, 
+                storage_context=storage_context,
+                transformations=[SentenceSplitter(chunk_size=1024, chunk_overlap=50)],
+            )
+        else:
+            # initialize client
+            db = chromadb.PersistentClient(path="./chroma_db/CUM_en_db")
+            # get collection
+            chroma_collection = db.get_or_create_collection("CUM_en_db")
+            # assign chroma as the vector_store to the context
+            vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+            storage_context = StorageContext.from_defaults(vector_store=vector_store)
+            # load your index from stored vectors
+            index = VectorStoreIndex.from_vector_store(
+                vector_store,
+                service_context=service_context, 
+                storage_context=storage_context
+            )
+ 
+        return index
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True, streaming=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+import time
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.stream_chat(prompt)
+            st.write_stream(response.response_gen)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
